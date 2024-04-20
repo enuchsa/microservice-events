17c13fbe097b4222765fbe5a210876c6a6f4d673
enuch <enuchsa@gmail.com>
2024-04-20 17:33:42 -0300

    Start project

---
+++ src/main/java/com/enuch/microserviceevents/MicroserviceEventsApplication.java
@@ -0,0 +1,13 @@
+package com.enuch.microserviceevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEventsApplication.class, args);
+    }
+
+}
