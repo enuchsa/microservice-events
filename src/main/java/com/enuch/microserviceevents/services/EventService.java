1a8d245dde2cf0139beb7936dd7d67280a6afdc4
enuch <enuchsa@gmail.com>
2024-04-20 22:08:51 -0300

    :sparkles: Feat: Event service

---
+++ src/main/java/com/enuch/microserviceevents/services/EventService.java
@@ -0,0 +1,60 @@
+package com.enuch.microserviceevents.services;
+
+import com.enuch.microserviceevents.DTOs.EmailRequestDTO;
+import com.enuch.microserviceevents.DTOs.EventRequestDTO;
+import com.enuch.microserviceevents.domain.Event;
+import com.enuch.microserviceevents.domain.Subscription;
+import com.enuch.microserviceevents.exceptions.EventFullException;
+import com.enuch.microserviceevents.exceptions.EventNotFoundException;
+import com.enuch.microserviceevents.repositories.EventRepository;
+import com.enuch.microserviceevents.repositories.SubscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+    @Autowired
+    private EmailServiceClient emailServiceClient;
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public List<Event> getUpcomingEvents() {
+        return eventRepository.findUpcomingEvents(LocalDateTime.now());
+    }
+
+    public Event createEvent(EventRequestDTO eventRequest) {
+        Event newEvent = new Event(eventRequest);
+        return eventRepository.save(newEvent);
+    }
+
+    private Boolean isEventFull(Event event){
+        return event.getRegisteredParticipants() >= event.getMaxParticipants();
+    }
+
+    public void registerParticipant(String eventId, String participantEmail) {
+        Event event = eventRepository.findById(eventId).orElseThrow(EventNotFoundException::new);
+
+        if(isEventFull(event)) {
+            throw new EventFullException();
+        }
+
+        Subscription subscription = new Subscription(event, participantEmail);
+        subscriptionRepository.save(subscription);
+
+        event.setRegisteredParticipants(event.getRegisteredParticipants() + 1);
+
+        EmailRequestDTO emailRequest = new EmailRequestDTO(participantEmail, "Confirmação de Inscrição", "Você foi inscrito no evento com sucesso!");
+
+        emailServiceClient.sendEmail(emailRequest);
+    }
+}
