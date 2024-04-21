bdf360738a757eb7d4767d862435162ed7475a66
enuch <enuchsa@gmail.com>
2024-04-20 22:09:15 -0300

    :sparkles: Feat: Event repository

---
+++ src/main/java/com/enuch/microserviceevents/repositories/EventRepository.java
@@ -0,0 +1,20 @@
+package com.enuch.microserviceevents.repositories;
+
+import com.enuch.microserviceevents.domain.Event;
+import jakarta.annotation.Nonnull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface EventRepository extends JpaRepository<Event, String> {
+
+    @Query(value = "SELECT * FROM events e WHERE parsedatetime(e.date AS TIMESTAMP) > :currentDate", nativeQuery = true)
+    List<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate);
+
+    @Nonnull
+    Optional<Event> findById(@Nonnull String id);
+}
